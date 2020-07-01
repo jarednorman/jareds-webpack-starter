3472990be8f5c052584e77009a69d438ba08a7ae
Jared Norman <jared@super.gd>
2020-07-01 16:50:44 -0700

    Set up webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    contentBase: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Replace Me',
