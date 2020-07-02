7ffb5972f33c6ebaa36c5c605645696fabb94379
Jared Norman <jared@super.gd>
2020-07-01 18:13:37 -0700

    Style page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import "./typography.css"
 
 const app = document.getElementById("app")
 app.innerHTML = `
-  <h1>Jared's Webpack Starter</h1>
-  <p>You can find out more <a href="https://github.com/jarednorman/jareds-webpack-starter">here</a>.</p>
+  <div style="padding: 1rem; display: flex; justify-content: center; align-items: center; min-height: 100vh;">
+    <div style="max-width: 30rem;">
+      <h1>Jared's Webpack Starter</h1>
+      <p>
+        You can find out more <a
+        href="https://github.com/jarednorman/jareds-webpack-starter">here</a>.
+      </p>
+    </div>
+  </div>
 `
